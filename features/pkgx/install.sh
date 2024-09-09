599c907699f55705fe54bee9240ef49c91a4ecb2
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-09-09 15:50:04 +0200

    pkgx

--- features/pkgx/install.sh
+++ features/pkgx/install.sh
@@ -10,15 +10,15 @@
 #   exit 1
 # fi
 
-# declare -a requiredAptPackagesMissing=()
+declare -a requiredAptPackagesMissing=()
 
 # if ! [ -r '/etc/ssl/certs/ca-certificates.crt' ]; then
 #   requiredAptPackagesMissing+=('ca-certificates')
 # fi
 
-# if ! command -v curl >/dev/null 2>&1; then
-#   requiredAptPackagesMissing+=('curl')
-# fi
+if ! command -v curl >/dev/null 2>&1; then
+  requiredAptPackagesMissing+=('curl')
+fi
 
 # if ! command -v tar >/dev/null 2>&1; then
 #   requiredAptPackagesMissing+=('tar')
@@ -32,22 +32,22 @@
 #   requiredAptPackagesMissing+=('jq')
 # fi
 
-# declare -i requiredAptPackagesMissingCount=${#requiredAptPackagesMissing[@]}
-# printf '=== Need to install %s required apt packages\n' \
-#   "${requiredAptPackagesMissingCount}"
+declare -i requiredAptPackagesMissingCount=${#requiredAptPackagesMissing[@]}
+printf '=== Need to install %s required apt packages\n' \
+  "${requiredAptPackagesMissingCount}"
 
-# if [ $requiredAptPackagesMissingCount -gt 0 ]; then
-#   printf '=== Run apt-get update\n'
-#   apt-get update
+if [ $requiredAptPackagesMissingCount -gt 0 ]; then
+  printf '=== Run apt-get update\n'
+  apt-get update
 
-#   printf '=== Install required apt packages "ca-certificates", "curl" and "tar"\n'
-#   apt-get install \
-#     --option 'Debug::pkgProblemResolver=true' \
-#     --option 'Debug::pkgAcquire::Worker=1' \
-#     --yes \
-#     --no-install-recommends \
-#     --no-install-suggests \
-#         "${requiredAptPackagesMissing[@]}"
-# fi
+  printf '=== Install required apt packages "ca-certificates", "curl" and "tar"\n'
+  apt-get install \
+    --option 'Debug::pkgProblemResolver=true' \
+    --option 'Debug::pkgAcquire::Worker=1' \
+    --yes \
+    --no-install-recommends \
+    --no-install-suggests \
+        "${requiredAptPackagesMissing[@]}"
+fi
 
 curl -fsS https://pkgx.sh | sh
