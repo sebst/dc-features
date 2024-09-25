2c39b2fcf490c9c0c0c9d8a96282d0e6ad05cb08
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-09-25 18:22:08 +0200

    add: `zz-double-install-test`

---
+++ features/zz-double-install-test/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset
+
+readonly name='zz-double-install-test'
+readonly executableName="${EXECNAME}"
+
+
+readonly targetPath="/usr/local/bin/${executableName}"
+
+echo "#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset" > "${targetPath}"
+echo "echo 'Hello, World!'" >> "${targetPath}"
+echo 'echo $0' >> "${targetPath}"
+chmod +x "${targetPath}"
+
+
+printf '=== [Success] Feature "%s" installed.\n' \
+  "${name}"
