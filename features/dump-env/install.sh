67753ba4973a96ef565f4e937a7985ab5ad64b8a
Bas Steins <sebst@users.noreply.github.com>
2024-10-01 21:10:22 +0200

    dump-env,dump-env-2,test-init,test-init-2

---
+++ features/dump-env/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset
+
+
+echo "Dumping environment variables..."
+env > /tmp/feature-dumpenv-env.txt
---
+++ features/test-init/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset
+
+echo "Installing entrypoint..."
+
+install -v -m 755 ./entrypoint.sh /usr/local/bin/entrypoint-test
