b00943523963fa4e9fa6410618a92ebe0a1f5229
Bas Steins <sebst@users.noreply.github.com>
2024-10-08 13:22:11 +0000

    feat(xserver): ready?

--- features/xserver/install.sh
+++ features/xserver/install.sh
@@ -116,43 +116,7 @@ fi
 mkdir -p /var/run/dbus /usr/local/etc/vscode-dev-containers/
 
 # Script to change resolution of desktop
-cat << EOF > /usr/local/bin/set-resolution
-#!/bin/bash
-RESOLUTION=\${1:-\${VNC_RESOLUTION:-1920x1080}}
-DPI=\${2:-\${VNC_DPI:-96}}
-IGNORE_ERROR=\${3:-"false"}
-if [ -z "\$1" ]; then
-    echo -e "**Current Settings **\n"
-    xrandr
-    echo -n -e "\nEnter new resolution (WIDTHxHEIGHT, blank for \${RESOLUTION}, Ctrl+C to abort).\n> "
-    read NEW_RES
-    if [ "\${NEW_RES}" != "" ]; then
-        RESOLUTION=\${NEW_RES}
-    fi
-    if ! echo "\${RESOLUTION}" | grep -E '[0-9]+x[0-9]+' > /dev/null; then
-        echo -e "\nInvalid resolution format!\n"
-        exit 1
-    fi
-    if [ -z "\$2" ]; then
-        echo -n -e "\nEnter new DPI (blank for \${DPI}, Ctrl+C to abort).\n> "
-        read NEW_DPI
-        if [ "\${NEW_DPI}" != "" ]; then
-            DPI=\${NEW_DPI}
-        fi
-    fi
-fi
-
-xrandr --fb \${RESOLUTION} --dpi \${DPI} > /dev/null 2>&1
-
-if [ \$? -ne 0 ] && [ "\${IGNORE_ERROR}" != "true" ]; then
-    echo -e "\nFAILED TO SET RESOLUTION!\n"
-    exit 1
-fi
-
-echo -e "\nSuccess!\n"
-EOF
-
-chmod +x /usr/local/bin/set-resolution
+install -v -m 755 ./bin/set-resolution.sh /usr/local/bin/set-resolution
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
